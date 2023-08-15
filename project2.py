bbfefe71ca484d9f7395e610fa0772268e6f647a
rikesh009 <rikesh.punwatkar@rediffmail.com>
2023-08-15 19:36:25 +0530

    python automation

---
+++ project2.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+time.sleep(6)
+element_username = driver.find_element(By.NAME,"username")
+element_username.send_keys("Admin")
+element_password = driver.find_element(By.NAME,"password")
+element_password.send_keys("admin12345")
+element_button= driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button')
+element_button.click()
+massage= driver.find_element(By.CLASS_NAME, "invalid credentials").text
+print(massage)
+driver.close()
